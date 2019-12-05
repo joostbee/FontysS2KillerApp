d2acf65b7bb1bb7fa78f473e830265ada57efdc2
joostbee <j.burggraaff@student.fontys.nl>
2019-12-05 09:38:21 +0100

    HelloWorld

---
+++ FontysKillerAppS2/FontysKillerAppS2/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace FontysKillerAppS2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-controller?view=aspnetcore-3.0&tabs=visual-studio
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ FontysKillerAppS2/FontysKillerAppS2/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FontysKillerAppS2.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Browse()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
